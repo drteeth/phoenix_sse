161b9292705e6e3bad3808fa1e338b7c189fb71d
Benjamin Moss <drteeth@gmail.com>
2019-12-24 10:28:46 -0500

    Add Http2 support

--- config/dev.exs
+++ config/dev.exs
@@ -17,6 +17,12 @@ config :phoenix_sse, PhoenixSse.Repo,
 # with webpack to recompile .js and .css sources.
 config :phoenix_sse, PhoenixSseWeb.Endpoint,
   http: [port: 4000],
+  https: [
+    port: 4001,
+    cipher_suite: :strong,
+    certfile: "priv/cert/selfsigned.pem",
+    keyfile: "priv/cert/selfsigned_key.pem"
+  ],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
