9f4fb1296e095165e5122b0485f7befc963d8946
Benjamin Moss <drteeth@gmail.com>
2020-03-06 21:37:34 -0500

    style it up

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,15 +3,24 @@ import css from "../css/app.css"
 import "phoenix_html"
 
 window.addEventListener('DOMContentLoaded', () => {
+  let idEl = document.getElementById("message-id");
+  let valueEl = document.getElementById("stock-value");
+  let symbolEl = document.getElementById("stock-symbol");
+  let statusEl = document.getElementById("status");
+
   let source = new EventSource("/sse");
 
   source.addEventListener('error', e => {
+    statusEl.innerText = e;
     console.log("error", e);
     console.log('SSE state:', source.readyState);
   });
 
   source.addEventListener('message', e => {
-    console.log("message", e.data);
+    let event = JSON.parse(e.data);
+    idEl.innerText = event.id;
+    symbolEl.innerText = event.symbol;
+    valueEl.innerText = event.value;
   });
 
   window.addEventListener("beforeunload", () => {
